93c2f6bc151629961f855c7ed3596f1c291092bc
Md. Ashiq Uddin Pranto <pranto@Mds-MacBook-Air.local>
2025-01-13 09:07:37 +0600

    project initialization, configured properties for db and added User entity

---
+++ src/main/java/com/example/WorkoutTrackingApp/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.WorkoutTrackingApp.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER; // Default role
+
+    private boolean isActive = true; // For enabling/disabling user accounts
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public enum Role {
+        ADMIN,
+        USER
+    }
+}
---
+++ src/test/java/com/example/WorkoutTrackingApp/WorkoutTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkoutTrackingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
